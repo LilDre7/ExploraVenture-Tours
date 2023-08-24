017288b474200644c4fc00e227f7926a22ac6f89
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-08-23 22:41:52 -0600

    :memo: upadate document

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,82 @@
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
+
+const ModelName = require("../models/ModelFile");
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access", 401)
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(
+    token,
+    process.env.SECRET_JWT_SEED
+  );
+
+  const user = await ModelName.findOne({
+    where: {
+      id: decoded.id,
+      status: true,
+    },
+  });
+
+  if (!user) {
+    return next(
+      new AppError("The owner of this token it not longer available", 401)
+    );
+  }
+
+  //only if you have the functionality to change password
+  /*
+  if (user.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      const changedTimeStamp = parseInt(
+      10
+    );
+
+    if (decoded.iat < changedTimeStamp) {
+      return next(
+        new AppError(
+          'User recently changed password!, please login again.',
+          401
+        )
+      );
+    }
+  }
+  */
+
+  req.sessionUser = user;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.sessionUser.role)) {
+      return next(
+        new AppError("You do not have permission to perfom this action.!", 403)
+      );
+    }
+
+    next();
+  };
+};
+
+exports.protectAccountOwner = catchAsync(async (req, res, next) => {
+  const { user, sessionUser } = req;
+
+  if (user.id !== sessionUser.id) {
+    return next(new AppError("You do not own this account.", 401));
+  }
+
+  next();
+});
