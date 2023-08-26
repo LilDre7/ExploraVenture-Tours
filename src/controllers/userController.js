05611e7e1d9dbb55bb0cf32b96d14718386dddb0
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-08-26 14:15:24 -0600

    :memo: upadate document

---
+++ src/controllers/userController.js
@@ -0,0 +1,6 @@
+const catchAsync = require("../utils/catchAsync");
+const USER = require("../models/userModel");
+
+exports.signup = catchAsync(async (req, res, next) => {});
+exports.login = catchAsync(async (req, res, next) => {});
+exports.renew = catchAsync(async (req, res, next) => {});
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-// 🦴 CONTROLLER 🦴 //
+// ** 🪢 AUTH MIDDLEWARE 🪢 ** //  ㅤ
 
-// 🧨 AUTH ROUTE 🧨 //
+// ** 🦴 AUTH CONTROLLER 🦴 ** //
+const authController = require("../controllers/userController");
+
+// ** 🧨 AUTH ROUTE 🧨 ** //
+router.route("/signup").post(authController.signup);
+router.route("/login").post(authController.login);
+router.route("/renew").get(authController.renew);
+
+//                                        //
+
+// ** 🪢 USER MIDDLEWARE 🪢  ** //
+
+// ** 🦴 USER CONTROLLER 🦴  ** //
+
+// ** 🧨 USER  ROUTE 🧨  ** //
 
 module.exports = router;
