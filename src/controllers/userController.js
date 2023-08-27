d3f93f28f637510e60982f658b9f14d074d00933
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-08-27 16:55:30 -0600

    :memo: upadate document

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,32 +3,42 @@ const AppError = require("../utils/appError");
 const USER = require("../models/userModel");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  // const { name, email, photo, password } = req.body;
-  // const userExisten = await USER.findOne({
-  //   where: {
-  //     email: email,
-  //   },
-  // });
-  // if (userExisten)
-  //   next(
-  //     new AppError(
-  //       `El email deber unico este ya existe: ${email} 🌞 Intenta con otro email 🌱 `,
-  //       400
-  //     )
-  //   );
-  // const newUser = await USER.create({
-  //   name,
-  //   email,
-  //   photo,
-  //   password,
-  // });
-  // res.status(201).json({
-  //   status: "success",
-  //   message: "User created successfully",
-  //   data: {
-  //     newUser,
-  //   },
-  // });
+  const { name, email, photo, password } = req.body;
+  const userExisten = await USER.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  if (userExisten)
+    next(
+      new AppError(
+        `El email deber unico este ya existe: ${email} 🌞 Intenta con otro email 🌱 `,
+        400
+      )
+    );
+
+  // ** Encriptar la contreña del usuario ** //
+  const salt = await bcrypt.genSalt(10);
+  console.log(salt);
+
+  const newUser = await USER.create({
+    name,
+    email,
+    photo,
+    password,
+  });
+
+  res.status(201).json({
+    status: "success",
+    message: "User created successfully",
+    data: {
+      newUser,
+    },
+  });
 });
-exports.login = catchAsync(async (req, res, next) => {});
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+});
+
 exports.renew = catchAsync(async (req, res, next) => {});
