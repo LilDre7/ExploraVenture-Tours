f6d2ba89c47ba519c3396c7eb82b04de24276dce
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-08-31 19:52:21 -0600

    :memo: upadate document

--- src/routes/reviewsRoute.js
+++ src/routes/reviewsRoute.js
@@ -2,4 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
+//                                        //
+
+// ** 🪢 USER MIDDLEWARE 🪢  ** //
+// const authMiddleware = require("../middleware/authMiddleware");
+// const tourValidate = require("../middleware/toursValidate");
+
+// ** 🦴 USER CONTROLLER 🦴  ** //
+// const tourController = require("../controllers/tourController");
+
+// 🎈 ----------------------- 🎈 //
+// router.use(authMiddleware.protect);
+// 🎈 ----------------------- 🎈 //
+
+// ** 🧨 USER  ROUTE 🧨  ** //
+
 module.exports = router;
