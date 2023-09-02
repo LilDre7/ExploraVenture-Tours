b52171d0debe11958c00ce020345ede6a92ed72d
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-01 20:54:35 -0600

    :memo: upadate document

--- src/controllers/reviewsController.js
+++ src/controllers/reviewsController.js
@@ -1,8 +1,16 @@
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {});
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const allReviews = await Review.findAll();
+
+  res.status(200).json({});
+});
+
 exports.getReviewForId = catchAsync(async (req, res, next) => {});
+
 exports.createReviewForTour = catchAsync(async (req, res, next) => {});
+
 exports.updateReviewTour = catchAsync(async (req, res, next) => {});
+
 exports.deleteReviewTour = catchAsync(async (req, res, next) => {});
