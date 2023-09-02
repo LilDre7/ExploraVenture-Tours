36b82df400a4c725b0bdbdc2607bdcaaec2fad7d
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-01 20:57:29 -0600

    :seedling: Add or update seed files

--- src/controllers/reviewsController.js
+++ src/controllers/reviewsController.js
@@ -1,10 +1,19 @@
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const Review = require("../models/reviewsModel");
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   const allReviews = await Review.findAll();
 
-  res.status(200).json({});
+  if (allReviews > 0) next(new AppError(" 🧨 No hay reviews realizadas 🧨 "));
+
+  res.status(200).json({
+    status: "success",
+    message: "Aqui todas las reviews creada por tu perfil 🧍🏾‍♂️🔍 ",
+    reviews: {
+      allReviews: allReviews,
+    },
+  });
 });
 
 exports.getReviewForId = catchAsync(async (req, res, next) => {});
